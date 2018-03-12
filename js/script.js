cba3e8fdf9aa0b95160b5afe52de39a95bae7331
Steve Martin <stevemartinrealestate@gmail.com>
2018-03-12 13:50:04 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,10 +31,12 @@ var express = document.querySelector(`[name="express"]`);
 var node = document.querySelector(`[name="node"]`);
 var build = document.querySelector(`[name="build-tools"]`);
 var npm = document.querySelector(`[name="npm"]`);
-//npm.setAttribute('disabled', 'disabled');
-var total;
+
+
 var activities = document.getElementsByClassName('activities')[0];
 var labels = document.querySelectorAll('.activities label');
+var activitiesList = document.querySelectorAll('.activities input');
+
 
 //   Validation ///
 
@@ -46,7 +48,8 @@ var cvv = document.querySelector('#cvv');
 var submit = document.querySelector(`[type="submit"]`);
 
 //////////////////////Select Different Payment Plans ////////////////////////
-var select = document.querySelector(`[value="select_method"]`);
+
+var selectMethod = document.querySelector(`[value="select_method"]`);
 var creditSelect = document.querySelector(`[value="credit card"]`);
 var paypalSelect = document.querySelector(`[value="paypal"]`);
 var bitcoinSelect = document.querySelector(`[value="bitcoin"]`);
@@ -56,26 +59,32 @@ var bitcoin = document.querySelectorAll('fieldset div p')[1];
 paypal.style.display = 'none';
 bitcoin.style.display = 'none';
 var payment = document.getElementById('payment');
+var runningTotal = document.getElementById('runningTotal')
 
 ////////////////////////////// Add an event listener to the title element on change
 
+//  Focuson User Name when the document loads
+
+userName.focus()
+
 var select = mainTitle.addEventListener('change', function(event){
-	if(event.target.value === title.value){
-	otherTitle.style.display='block';
-}
-    else if(event.target.value !== title.value){
-        otherTitle.style.display='none';
-    }
+    	if(event.target.value === title.value){
+    	otherTitle.style.display='block';
+      }
+      else if(event.target.value !== title.value){
+            otherTitle.style.display='none';
+      }
 });
 
 //  Hide Element and run function 
 var hide = document.getElementById('color').options;
-for (i=0; i<hide.length; i++){
+    for (i=0; i<hide.length; i++){
     hide[i].style.display = 'none';
 }
 
 ////////////Function to Populate Function /////////////////////////////////
-    function populateColors(main , contingent){
+function populateColors(main , contingent){
+
       var main =  document.getElementById(main);
       var contingent =  document.getElementById(contingent);
       contingent.innerHTML = "";
@@ -86,197 +95,315 @@ for (i=0; i<hide.length; i++){
         var colorsArray = [ "tomato|Tomato (I &#9829; JS shirt only)", 'steelblue|Steel Blue (I &#9829; JS shirt only)', "dimgrey|Dim Grey (I &#9829; JS shirt only)"]; 
       }
       for(var colors in colorsArray){
-        var pair = colorsArray[colors].split("|");
-        var newOptions = document.createElement('option');
-        newOptions.value = pair[0];
-        newOptions.innerHTML = pair[1];
-        contingent.options.add(newOptions);
+            var pair = colorsArray[colors].split("|");
+            var newOptions = document.createElement('option');
+            newOptions.value = pair[0];
+            newOptions.innerHTML = pair[1];
+            contingent.options.add(newOptions);
       }
-    }
+}
+
+///Variables for Running Totals
+
+var total = '';
+var sumAll = '';
+var sumFrameworks = '';
+var sumLibs = '';
+var sumExpress = '';
+var sumNode = '';
+var sumBuild = '';
+var sumNpm = ''; 
 
 //////////////////////Event Lisener for Activities and Disable some buttons /////////////////////
 activities.addEventListener('change', function(event){  
 
-          if (((event.target.name == frameworks.name) && (event.target.checked)) || ((event.target.name == libs.name) && (event.target.checked))){
-              express.setAttribute('disabled', true);
-              node.setAttribute('disabled', true);  
+          if (((event.target.name == frameworks.name) && (event.target.checked)) ){
+              express.setAttribute('disabled', true);   
+              total =+ 100;  
           } 
-          else if (((event.target.name == express.name) && (event.target.checked)) || ((event.target.name == node.name) && (event.target.checked))){   
-              frameworks.setAttribute('disabled', true);
-              libs.setAttribute('disabled', true);  
+          else if (!frameworks.checked) {
+            express.removeAttribute('disabled');   
+             
           } 
-          else if (((!frameworks.checked && !libs.checked)) && ((!express.checked && !node.checked)) ){
-            express.removeAttribute('disabled');
-            node.removeAttribute('disabled');
-            frameworks.removeAttribute('disabled');
-            libs.removeAttribute('disabled');
+
+           if (((event.target.name == libs.name) && (event.target.checked))){      
+              node.setAttribute('disabled', true); 
+              total =+ 100; 
+          }
+          else if (!libs.checked){
+            node.removeAttribute('disabled'); 
+                 
           }
 
-          if (all.checked){
-            sumAll =200;
-          }else{
+          if (((event.target.name == express.name) && (event.target.checked))){      
+              frameworks.setAttribute('disabled', true);  
+                 total =+ 100; 
+          } 
+           else if (!express.checked) {
+            frameworks.removeAttribute('disabled'); 
+               
+          }
+           if (((event.target.name == node.name) && (event.target.checked))){      
+              libs.setAttribute('disabled', true);   
+                 total =+ 100;     
+          } 
+            else if (!node.checked) {
+            libs.removeAttribute('disabled'); 
+                        
+          }   
+
+          if (all.checked) {
+            sumAll = parseInt(200);
+            
+          }  
+          else{
             sumAll = 0;
           }
-           if(frameworks.checked){
-            sumFrameworks = 100;
-           }else{
-            sumFrameworks =0;
-           }
-           if(libs.checked){
-            sumLibs = 100;
-           }else{
+
+           if (frameworks.checked) {
+            sumFrameworks = parseInt(100);
+           
+          }  
+          else{
+            sumFrameworks = 0;
+          }
+
+           if (libs.checked) {
+            sumLibs = parseInt(100);
+           
+          }  
+          else{
             sumLibs = 0;
-           }
-          if(express.checked){
-            sumExpress = 100;
-           }else{
+          }
+
+           if (express.checked) {
+            sumExpress = parseInt(100);
+           
+          }  
+          else{
             sumExpress = 0;
-           }
-           if(node.checked){
-            sumNode = 100;
-           }else{
+          }
+
+           if (node.checked) {
+            sumNode = parseInt(100);
+           
+          }  
+          else{
             sumNode = 0;
-           }
-           if(build.checked){
-            sumBuild = 100;
-           }else{
+          }
+
+           if (build.checked) {
+            sumBuild = parseInt(100);
+           
+          }  
+          else{
             sumBuild = 0;
-           }
-           if(npm.checked){
-            sumNpm = 100;
-           }else{
+          }
+
+          if (npm.checked) {
+            sumNpm = parseInt(100);
+          }  
+          else{
             sumNpm = 0;
-           }
-           total = (sumAll + sumFrameworks + sumLibs + sumExpress + sumNode +sumBuild + sumNpm)
-          var yourTotal = document.createElement('span');
-           yourTotal.innerHTML = total + ' ';
-           activities.appendChild(yourTotal);
+          }
+
+          total = (sumAll + sumFrameworks + sumLibs + sumExpress + sumNode + sumBuild + sumNpm)
+          runningTotal.innerHTML = "Total: " +total;
 });
 
 
 /////Event Listener when user selects payment options
 
-payment.addEventListener('click', function(e){
-  if(e.target.value === paypalSelect.value ){
-    creditCard.style.display ='none';
-    paypal.style.display ='block';
-    bitcoin.style.display = 'none';
+payment.addEventListener('change', function(e){
+  if(e.target.value === selectMethod.value ){
+      creditCard.style.display ='none';
+      paypal.style.display ='none';
+      bitcoin.style.display = 'none';
   }
+  else if(e.target.value === paypalSelect.value ){
+      creditCard.style.display ='none';
+      paypal.style.display ='block';
+      bitcoin.style.display = 'none';
+  }
+
   else if(  e.target.value === bitcoinSelect.value   ){
       creditCard.style.display ='none';
       bitcoin.style.display = 'block';
       paypal.style.display ='none';
   }
   else if( e.target.value === creditSelect.value    ){
-       creditCard.style.display ='block';
+      creditCard.style.display ='block';
       bitcoin.style.display = 'none';
       paypal.style.display ='none';
   }
+  else{
+      creditCard.style.display ='none';
+      paypal.style.display ='none';
+      bitcoin.style.display = 'none';
+  }
+
 });
 
 ////////////  Event Listener for when user presses submit button ////////////////////////
 
 submit.addEventListener('click', function(e){
-      e.preventDefault();
+     
          if (userName.value == '' ){
-            alert('You must fill our your username');
-            nameError.style.display='block';
-            }         
+                alert('You must fill our your username');
+                 e.preventDefault();
+                nameError.style.display='block';
+          }else if (userName.value !== '' ){
+                nameError.style.display='none';
+          } 
+
         if(email.value == '' ){
             alert('You must fill our your email'); 
             emailError.style.display='block'; 
-            }
+             e.preventDefault();
+        }else if (email.value !== '' ){
+                emailError.style.display='none';
+        }
 
         if(!all.checked && !frameworks.checked && !libs.checked && !express.checked && !node.checked  && !build.checked && !npm.checked){
                alert('You must choose an activity'); 
               activitiesError.style.display='block'; 
-        }else{
-              //alert("big bird")
-            }
+               e.preventDefault();
+        }
 
-        if(creditSelect.value && !paypalSelect.value || !bitcoinSelect.value){
+        else if (all.checked || frameworks.checked || libs.checked || express.checked || node.checked  || build.checked || npm.checked){
+                 activitiesError.style.display='none'; 
+                
+        }
+
+function elValidator(){
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)){
+                  
+                  return (true)
+                }else{
+                     e.preventDefault();
+                  alert("You have entered an invalid email address!")
+                  return (false)            
+                }
+          }
+elValidator();
+
+if(creditSelect.value && !paypalSelect.value || !bitcoinSelect.value){
+ 
+            
              if(ccNum.value == '' || zip.value == '' || cvv.value == ''  ){
                   alert('you are missing some credit card info ');
+                     e.preventDefault();
 
                 if(isNaN(ccNum.value) == true ){
                         alert('Your Credit Card must only have numbers ')
+                         e.preventDefault();
                     } 
                   if(isNaN(zip.value) == true ){
                         alert('Your Zip must only have numbers ')
+                         e.preventDefault();
                     } 
                   if(isNaN(cvv.value) == true ){
                         alert('Your CVV must only have numbers ')
+                         e.preventDefault();
                  }     
             }
         }
-     
-        if(creditSelect.value){
+      
+           
+            
+    if(creditSelect.value ){
           if(ccNum.value == ''){
-            ccNumError.style.display='block'; 
+             e.preventDefault();
+             alert('You must fill in a card number');
+              ccNumError.style.display='block'; 
           }
-        }
-        if(creditSelect.value){
+          else if (ccNum.value !== ''){
+             ccNumError.style.display='none'; 
+          }
+        
+
+        
           if(zip.value == ''){
+             e.preventDefault();
+              alert('You must fill in a zip code');
             zipError.style.display='block'; 
           }
-        }
-        if(creditSelect.value){
+          else {
+            zipError.style.display='none'; 
+          }
+        
+
+        
+           
           if(cvv.value == ''){
+            e.preventDefault();
+             alert('You must fill in a cvv number');
             cvvError.style.display='block'; 
           }
-        }
-      
-
+    }
+    
 
         if(creditSelect.value ){
-             if(ccNum.value !== ''){
-                  
+          if(ccNum.value !== ''){
+                
                 if(isNaN(ccNum.value) == true ){
                         alert('Your Credit Card must only have numbers ');
+                         e.preventDefault();
+                        ccNumError.style.display='block'; 
                     } 
                 if(ccNum.value.length < 13 || ccNum.value.length>16 ){
+                        e.preventDefault();
                         
                         alert('You have too few or too many numbers');
                         ccNumError.style.display='block'; 
                     } 
+                    else{
+                        ccNumError.style.display='none'; 
+                    }
                 }
-              }
+            }
 
         if(creditSelect.value){
              if(zip.value !== ''){
                   
                 if(isNaN(zip.value) == true ){
+                       e.preventDefault();
                         alert('Your zip must only have numbers ');
+                        zipError.style.display='block'; 
                     } 
                   else if( isNaN(zip.value)== false && zip.value.length !== 5){
+                       e.preventDefault();
                         alert('Your Zip Code must only have 5 digits');
+                        zipError.style.display='block'; 
                     }  
-                }
+                    else{
+                        zipError.style.display='none'; 
+                    }
               }
+        }
 
         if(creditSelect.value){
              if(cvv.value !== ''){
                   
                 if(isNaN(cvv.value) == true ){
+                         e.preventDefault();
                         alert('Your cvv must only have numbers ');
+                        cvvError.style.display='block'; 
                     } 
                   else if( isNaN(cvv.value)== false && cvv.value.length !== 3){
+                       e.preventDefault();
                         alert('Your cvv Code must only have 3 digits');
+                        cvvError.style.display='block'; 
                     } 
+                    else{
+                      cvvError.style.display='none';
+                    }
                 }
-              }
-          function elValidator(){
-                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)){
-                  
-                  return (true)
-                }else{
-                  alert("You have entered an invalid email address!")
-                  return (false)            
-                }
-          }
-          elValidator();
+              }         
 
 //Closing Tag for Submit Hangler
+
+
 });
 
 
